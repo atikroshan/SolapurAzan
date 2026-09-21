7297c9f37dd897e7ed81833d328a57029aadaf3b
atikroshan <atikroshan2911@gmail.com>
2026-09-21 21:49:47 +0530

    chore: revert version to 1.1.0
    Update version code and name to align with the 1.1.0 release track.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aistudio.offlineazan.aqzwsx"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
