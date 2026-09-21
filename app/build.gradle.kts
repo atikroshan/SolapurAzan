030b28fed7a063850b78636fdb4345531b359695
atikroshan <atikroshan2911@gmail.com>
2026-09-21 22:07:38 +0530

    chore: bump version to 2.0.0
    Update version code to 5 and version name to 2.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aistudio.offlineazan.aqzwsx"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
